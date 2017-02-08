398963ad346b24267e5cef7d76962f9b52ebdfd7
Ben Kinsey <ben.kinsey@altres.com>
2017-02-07 14:27:50 -1000

    feat(server): implement example for each kind of CRUD query/mutation

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -17,8 +17,23 @@ const typeDefs = gql`
     users: [User]
     user(id: Int): User
   }
+  type Mutation {
+    createUser(
+      firstName: String!, 
+      lastName: String!
+    ): User
+    deleteUser(
+      id: Int!
+    ): Boolean
+    updateUser(
+      id: Int!
+      firstName: String
+      lastName: String
+    ): User
+  }
   schema {
     query: Query
+    mutation: Mutation
   }
 `;
 
@@ -33,9 +48,31 @@ const resolvers = {
     async user(root, args, context) {
       return await context.connection
         .getRepository(AppUser)
-        .createQueryBuilder('user')
-        .where(`user.id = :id`, { id: args.id })
-        .getOne();
+        .findOneById(args.id);
+    }
+  },
+  Mutation: {
+    async createUser(root, args, context) {
+      const user = new AppUser();
+      user.firstName = args.firstName;
+      user.lastName = args.lastName;
+      await context.connection.entityManager.persist(user);
+      return user;
+    },
+    async deleteUser(root, args, context) {
+      const repository = context.connection.getRepository(AppUser);
+      const user = await repository.findOneById(args.id);
+      if (!user) { throw new Error('No user found'); }
+      await repository.remove(user);
+      return true;
+    },
+    async updateUser(root, args, context) {
+      const repository = context.connection.getRepository(AppUser);
+      const user = await repository.findOneById(args.id);
+      if (!user) { throw new Error('No user found'); }
+      Object.assign(user, args);
+      await repository.persist(user);
+      return user;
     }
   }
 };
