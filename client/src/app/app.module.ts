6dd4b2b87e98c33ce5bc145bf9c57b8fc613c08d
Ben Kinsey <ben.kinsey@altres.com>
2017-03-06 19:42:40 -1000

    feat(client): add routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
